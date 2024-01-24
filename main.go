e888a806edc81f19bae00438bea471219146b033
gguinea 🦫 <kamil5176@gmail.com>
2024-01-24 21:55:15 +0100

    GIN as default router

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import ("fmt")
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello from devices!")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Pong",
+		},)
+	})
+	router.Run("localhost:8080")
 }
