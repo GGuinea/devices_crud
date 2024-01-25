d1c34fcdcca6f27d23279cc0e1c342a4654d695a
gguinea 🦫 <kamil5176@gmail.com>
2024-01-25 19:12:04 +0100

    Mock implementation

--- internal/devices/app/ports/devicesRepository.go
+++ internal/devices/app/ports/devicesRepository.go
@@ -3,6 +3,7 @@ package ports
 import (
 	"context"
 	"devices_crud/internal/devices/domain"
+	"strings"
 )
 
 type DevicesRepository interface {
@@ -13,4 +14,60 @@ type DevicesRepository interface {
 	Patch(ctx context.Context, device *domain.Device) (*domain.Device, error)
 	Delete(ctx context.Context, id string) error
 	Search(ctx context.Context, query string) ([]domain.Device, error)
+}
+
+type devicesRepositoryMock struct {
+	Devices map[string]domain.Device
+}
+
+func NewDevicesRepositoryMock() DevicesRepository {
+	return &devicesRepositoryMock{
+		Devices: make(map[string]domain.Device),
+	}
+}
+
+func (r *devicesRepositoryMock) Save(ctx context.Context, device *domain.Device) (*domain.Device, error) {
+	r.Devices[device.ID] = *device
+	return device, nil
+}
+
+func (r *devicesRepositoryMock) FindByID(ctx context.Context, id *string) (*domain.Device, error) {
+	device, ok := r.Devices[*id]
+	if !ok {
+		return nil, nil
+	}
+	return &device, nil
+}
+
+func (r *devicesRepositoryMock) FindAll(ctx context.Context) ([]domain.Device, error) {
+	var devices []domain.Device
+	for _, device := range r.Devices {
+		devices = append(devices, device)
+	}
+	return devices, nil
+}
+
+func (r *devicesRepositoryMock) Replace(ctx context.Context, device *domain.Device) (*domain.Device, error) {
+	r.Devices[device.ID] = *device
+	return device, nil
+}
+
+func (r *devicesRepositoryMock) Patch(ctx context.Context, device *domain.Device) (*domain.Device, error) {
+	r.Devices[device.ID] = *device
+	return device, nil
+}
+
+func (r *devicesRepositoryMock) Delete(ctx context.Context, id string) error {
+	delete(r.Devices, id)
+	return nil
+}
+
+func (r *devicesRepositoryMock) Search(ctx context.Context, query string) ([]domain.Device, error) {
+	var devices []domain.Device
+	for _, device := range r.Devices {
+		if strings.ToLower(device.DeviceBrand) == strings.ToLower(query) {
+			devices = append(devices, device)
+		}
+	}
+	return devices, nil
 }
